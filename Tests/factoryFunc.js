6b975f4f5d5e8df4e8bc69d446604a93eac4102d
Raadiyah Arend <raadiyaharend6@gmail.com>
2023-06-12 16:35:36 +0200

    added my factory functions

---
+++ Tests/factoryFunc.js
@@ -0,0 +1,98 @@
+function MainGreetings() {
+  var counter = 0;
+  var languageType = '';
+  var partyUsers = [];
+  var userName = '';
+  let errorMessage = '';
+
+  function setName(name) {
+    if (ValidateName(name)) {
+      if (!partyUsers.includes(name)) {
+        partyUsers.push(name)
+      }
+      userName = name
+    }
+
+  }//set the names and push them to an array
+
+  function getName() {
+    return userName;
+  }//getting a single name that was set
+
+  function getNames() {
+    return partyUsers;
+  }//getting the users
+
+  function setLanguage(language) {
+    languageType = language;
+  }//setting the different type of languages
+
+  function ValidateName(name) {
+    const regex = /^[A-Za-z]+$/;
+
+    return regex.test(name)
+  }
+  function greetingTheUser() {
+    let greetingMessage;
+
+    switch (languageType) {
+
+      case 'english':
+        greetingMessage = 'Hello, ' + userName + '!';
+        break;
+      case 'afrikaans':
+        greetingMessage = 'Hallo, ' + userName + '!';
+        break;
+      case 'arabic':
+        greetingMessage = 'مرحباً، ' + userName + '!';
+        break;
+    }
+
+    return greetingMessage
+  }
+
+  function errorMessages() {
+    //checking for the certain inputs that needed to be flled in or selected if not it return an error message.
+    if (!userName.match(/^[a-zA-Z]$/) && languageType === "") {
+      errorMessage = 'Please enter your name and select a language';
+    }
+    else if (languageType === "") {
+      errorMessage = 'Please select a language';
+    }
+    else if (!userName.match(/^[A-Za-z]+$/) === "") {
+      errorMessage = 'Please enter your name';
+    }
+    return errorMessage;
+  }
+  function setCounter() {
+
+  }
+
+  function resetCounter() {
+    counter = 0;
+    partyUsers= [];
+  }//reset the user counter num to 0
+
+  function getCounter() {
+    return counter;
+  }//get the amount of users 
+
+  function amountOfUsers() {
+    counter = partyUsers.length;
+    //get the amount of users by pushing all the users names in an array and the count each of them on the counter.
+    return counter
+  }
+
+  return {
+    errorMessages,
+    resetCounter,
+    getName,
+    getNames,
+    setName,
+    setLanguage,
+    getCounter,
+    amountOfUsers,
+    ValidateName,
+    greetingTheUser
+  }
+};
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //Selecting all the elements and buttons
+const greetingsApp = MainGreetings();
+
 const valueLanguages = document.querySelector(".itemTypeRadio");
 
 const enterButton = document.querySelector(".enter");
 
-const Name = document.querySelector(".Name");
+const nameElement = document.querySelector(".Name");
 
 const valueDisplay = document.querySelector(".valueDisplay");
 
@@ -13,73 +15,20 @@ const error = document.querySelector(".error");
 
 const theMessage = document.querySelector(".theMessage");
 
-let counter = 0;
-
-let NameTotal = 0;
-
-let NameBasket = "";
-
-let nameCheck = [];
 
 
+enterButton.addEventListener("click", function () {
 
-function incrementCounter() {
-  if(Name.value !== ""){
-    counter++;
-    valueDisplay.innerHTML = counter;
-  }
-  else if(Name.value !== nameCheck){
-    counter++;
-  }
-}
-enterButton.addEventListener("click", incrementCounter);
+  greetingsApp.setName(nameElement.value)
+  const languageType = document.querySelector('input[name="languageType"]:checked');
+  greetingsApp.setLanguage(languageType.value)
+  theMessage.innerHTML = greetingsApp.greetingTheUser();
+  valueDisplay.innerHTML = greetingsApp.amountOfUsers();
 
-resetBtn.addEventListener("click", () => {
-  counter = 0;
-  theMessage.remove();
-  valueDisplay.innerHTML = counter;
+})
 
-});
-
-enterButton.addEventListener("click", function(){ 
-
-  const checkedRadioBtn = document.querySelector("input[name='languageType']:checked");
-  
-  if (!checkedRadioBtn && !Name.value){
-    error.innerHTML = "Please enter your name and a language.";
-    }
- else if (!Name.value) {
-      error.innerHTML = "Please enter your name.";
-  }
-  
- else if (!checkedRadioBtn){
-    error.innerHTML = "Please select a language.";
-  
-  }
-  
-  });
-
-  function greetUser() {
-  
-    const checkedRadioBtn = document.querySelector("input[name='languageType']:checked");
-  if (checkedRadioBtn){
-      var greeting = checkedRadioBtn.value
-  }
-    
-      switch (greeting) {
-        case 'english':
-          greeting = 'Hello, ' + Name.value + '!';
-          break;
-        case 'afrikaans':
-          greeting = 'Haai, ' + Name.value + '!';
-          break;
-        case 'arabic':
-          greeting = 'مرحباً، ' + Name.value + '!';
-          break;
-      }
-    
-      // Display the greeting message
-      var messageDiv = document.querySelector('.theMessage');
-      messageDiv.textContent = greeting;
-  }
-  enterButton.addEventListener("click", greetUser);
+resetBtn.addEventListener("click", function () {
+  greetingsApp.resetCounter();
+  theMessage.innerHTML = greetingsApp.greetingTheUser();
+  valueDisplay.innerHTML = greetingsApp.amountOfUsers();
+})
